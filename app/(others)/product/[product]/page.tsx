0c56f0104faedad4120e9dd59cf9045a8c5d697d
YARNNNYBOI <marichcastro312@gmail.com>
2026-07-03 14:27:44 +0800

    lesson 20 not yet finished and not working

---
+++ app/(others)/product/[product]/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+
+type Props = {
+    params: Promise<{productId: string}>;
+};
+
+export const generateMetadata = async ({
+    params,
+}:  Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iphone ${id}`);
+        }, 100);
+    });
+    return {
+        title: `product ${title}`,
+    };
+};
+
+export default async function ProductDetails({ params }: Props){
+    const productId = (await params).productId;
+    return <h1>Details {productId} </h1>
+}
--- app/product/[product]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function productPage({
-    params,
-}: {
-    params: Promise<{productID: string}>;
-}) {
-    const {productID} = await params;
-    return <h1>{productID}</h1>;
-}
